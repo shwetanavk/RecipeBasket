59ef2123fd3c29961a11a7fabd3e274f69bdfdbc
shwetanavk <shwetanavkar18@gmail.com>
2024-09-20 19:07:30 +0530

    shopping list form using template driven form

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,32 +1,67 @@
 import {
   Component,
+  OnDestroy,
   OnInit,
-  ElementRef,
   ViewChild
 } from '@angular/core';
 
 import { Ingredient } from '../../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list.service';
+import { NgForm } from '@angular/forms';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
   styleUrls: ['./shopping-edit.component.css']
 })
-export class ShoppingEditComponent implements OnInit {
-  @ViewChild('nameInput', { static: false }) nameInputRef: ElementRef;
-  @ViewChild('amountInput', { static: false }) amountInputRef: ElementRef;
+export class ShoppingEditComponent implements OnInit, OnDestroy {
+  @ViewChild('f') slForm: NgForm;
+  subscription: Subscription;
+  editMode = false;
+  editedItemIndex: number;
+  editedItem: Ingredient;
 
   constructor(private shoppingListService: ShoppingListService) { }
-
+  
   ngOnInit() {
+    this.subscription = this.shoppingListService.startedEditing.subscribe((index: number) => {
+      this.editedItemIndex = index;
+      this.editMode = true;
+      this.editedItem = this.shoppingListService.getIngredient(index);
+      this.slForm.setValue({
+        name: this.editedItem.name,
+        amount: this.editedItem.amount
+      })
+    });
+  }
+  
+  onSubmit() {
+    const value = this.slForm.value;
+    const newIngredient = new Ingredient(value.name, value.amount);
+
+    if(!this.editMode)
+      this.shoppingListService.addNewIngredient(newIngredient);
+    else {
+      this.shoppingListService.updateIngredient(newIngredient, this.editedItemIndex)
+    }
+
+    this.onClearItem();
   }
 
-  onAddItem() {
-    const ingName = this.nameInputRef.nativeElement.value;
-    const ingAmount = this.amountInputRef.nativeElement.value;
-    const newIngredient = new Ingredient(ingName, ingAmount);
-    this.shoppingListService.addNewIngredient(newIngredient);
+  onClearItem() {
+    this.slForm.reset();
+    this.editMode = false;
   }
 
+  onDeleteItem(){
+    if(this.editMode) {
+      this.shoppingListService.deleteIngredient(this.editedItemIndex);
+    }
+    this.onClearItem();
+  }
+  
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 }
--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -5,6 +5,7 @@ import { Ingredient } from '../shared/ingredient.model';
 @Injectable()
 export class ShoppingListService { 
   ingredientsChanged = new Subject<Ingredient[]>();
+  startedEditing = new Subject<number>();
 
   private ingredients: Ingredient[] = [
     new Ingredient('Flour', 1),
@@ -14,16 +15,27 @@ export class ShoppingListService {
   getIngredients() {
     return this.ingredients.slice();
   }
+  
+  getIngredient(index: number) {
+    return this.ingredients[index];
+  }
 
   addNewIngredient(newIngredient: Ingredient) {
     this.ingredients.push(newIngredient);
     this.ingredientsChanged.next(this.ingredients.slice());
   }
+  
+  updateIngredient(ingredient: Ingredient, index: number) {
+    this.ingredients[index] = ingredient;
+    this.ingredientsChanged.next(this.ingredients.slice());
+  }
+
+  deleteIngredient(index: number){
+    this.ingredients.splice(index, 1);
+    this.ingredientsChanged.next(this.ingredients.slice());
+  }
 
   addNewIngredients(ings: Ingredient[]) {
-    // for (var item of ings) {
-    //   this.addNewIngredient(item);
-    // }
     this.ingredients.push(...ings);
     this.ingredientsChanged.next(this.ingredients.slice());
     alert("New ingredients added to shopping-list");
